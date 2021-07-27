1b97cbb37808df1d5edf6f8a6e67f9e2a174e388
Koussay <koussaydhifi48@gmail.com>
2021-07-27 18:03:36 +0100

    The Main file that combines the project components

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from entry import entry
+from Label import label
+from button import button
+window = tk.Tk()
+label("Enter Your task")
+entry()
+button()
+label("To Do:")
+
+
+for i in range(4):
+	pass
+
+window.mainloop()
